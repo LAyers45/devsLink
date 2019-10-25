9610f19483ed86f8a2adfb116fa0b26d2ad38d85
Sunny <loganayers2013@yahoo.com>
2019-10-24 20:22:42 -0500

    adding changes prior to pull

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./SignIn";
-import Footer from '../Footer/Footer'
+import Footer from '../../components/Footer/Footer'
 
 
 
--- src/Pages/UserPage/UserPage.js
+++ src/Pages/UserPage/UserPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./UserPage";
+import UserInfoCard from '../../components/UserInfoCard/UserInfoCard'
 
 
 
 function UserPage() {
     return (
-        <div>UserPage</div>
+        <div>
+            <UserInfoCard />
+        </div>
     )
 
 }
