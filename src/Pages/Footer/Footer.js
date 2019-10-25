b69dd7b7678b98b1b80627ae8ee481d310c3fd36
evandavies11 <evandavies11@gmail.com>
2019-10-24 19:23:24 -0500

    got dat footer

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Footer.css";
+
+
+
+function DevSearch() {
+    return (
+        <footer className="footer">
+            <span>@ devslink 2019</span>
+        </footer>
+    )
+
+}
+
+export default DevSearch;
