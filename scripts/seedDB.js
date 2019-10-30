afe8a36c8677a4d95701732367afa641a5d778b7
Sunny <loganayers2013@yahoo.com>
2019-10-29 20:00:20 -0500

    seeding database

---
+++ scripts/seedDB.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactreadinglist"
+);
+
+const userSeed = [
+  {
+    username: "LAyers45",
+    password: "Stephen King",
+    email: "loganayers2013@yahoo.com",
+    userCreated: new Date(Date.now()),
+    specialization:
+
+  },
+  {
+    title: "Lord of the Flies",
+    author: "William Golding",
+    synopsis:
+      "The tale of a party of shipwrecked schoolboys, marooned on a coral island, who at first enjoy the freedom of the situation but soon divide into fearsome gangs which turn the paradise island into a nightmare of panic and death.",
+    date: new Date(Date.now())
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    synopsis:
+      "The Catcher in the Rye is a 1951 novel by J. D. Salinger. A controversial novel originally published for adults, it has since become popular with adolescent readers for its themes of teenage angst and alienation. It has been translated into almost all of the world's major languages. Around 1 million copies are sold each year with total sales of more than 65 million books. The novel's protagonist Holden Caulfield has become an icon for teenage rebellion. The novel also deals with complex issues of innocence, identity, belonging, loss, and connection.",
+    date: new Date(Date.now())
+  }
+];
+
+db.Book
+  .remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
