0661da25543e0c55ef4f4f18cddb4edb8f0b36af
Ricardo Perdomo <hndgd1980@gmail.com>
2019-10-31 20:52:44 -0500

    main page cards loaded

--- client/src/Pages/MainPage/MainPage.js
+++ client/src/Pages/MainPage/MainPage.js
@@ -1,8 +1,15 @@
 import React from "react";
-import "./MainPage";
+import "./Main.css";
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
-import BackgroundImage from '../../images/background-devslink.png';
+import LogoMultiple from '../../images/logo-multiple.png';
+import DevSearch from '../../images/dev-search.png';
+import DevWorks from '../../images/dev-works.png';
+import UserProfile from '../../images/user-profile.png';
+import Project from '../../images/project.png';
+
+import { Container, Row, Col } from 'reactstrap';
+
 
 
 
@@ -12,7 +19,17 @@ function MainPage() {
     return (
         <div>
             <Navbar />
-            <img src={BackgroundImage} className="backgroundImage" alt=" devs link" />
+            <Container fluid>
+                <Row>
+                    <img src={LogoMultiple} className="logoMultiple" alt="logo multiple" />
+                    <Col lg="12">
+                        <img src={DevSearch} className="cards" id="cardSearch" alt="dev search" />
+                        <img src={DevWorks} className="cards" id="cardWorks" alt="dev works" />
+                        <img src={UserProfile} className="cards" id="cardUser" alt="profile" />
+                        <img src={Project} className="cards" id="cardProject" alt="project" />
+                    </Col>
+                </Row>
+            </Container>
             <Footer />
         </div>
     )
