b9c9517c4d9681622d87e26808b73c0fa1825100
Sunny <loganayers2013@yahoo.com>
2019-11-02 10:27:14 -0500

    commit for pull

--- models/user.js
+++ models/user.js
@@ -1,24 +1,16 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 var UserSchema = new Schema({
-    // `username` must be of type String
-    // `username` will trim leading and trailing whitespace before it's saved
-    // `username` is a required field and throws a custom error message if not supplied
+
     username: {
         type: String,
         trim: true,
         unique: true,
         required: "Username is Required"
     },
-    // `password` must be of type String
-    // `password` will trim leading and trailing whitespace before it's saved
-    // `password` is a required field and throws a custom error message if not supplied
-    // `password` uses a custom validation function to only accept values 6 characters or more
+
     password: {
         type: String,
         trim: true,
@@ -30,17 +22,13 @@ var UserSchema = new Schema({
             "Password should be longer."
         ]
     },
-    // `email` must be of type String
-    // `email` must be unique
-    // `email` must match the regex pattern below and throws a custom error message if it does not
-    // You can read more about RegEx Patterns here https://www.regexbuddy.com/regex.html
+
     email: {
         type: String,
         unique: true,
         required: "email is Required",
         match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
     },
-    // `date` must be of type Date. The default value is the current date
     userCreated: {
         type: Date,
         default: Date.now
@@ -55,8 +43,8 @@ var UserSchema = new Schema({
     }
 });
 
-// This creates our model from the above schema, using mongoose's model method
+
 var User = mongoose.model("User", UserSchema);
 
-// Export the User model
+
 module.exports = User;
