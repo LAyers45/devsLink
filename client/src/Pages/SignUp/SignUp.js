463060aaeb776685a07a81ddb99e0f06dc7abdd7
Ricardo Perdomo <hndgd1980@gmail.com>
2019-11-02 11:24:23 -0500

    animation on navbar,main page and styling sign up card

--- client/src/Pages/SignUp/SignUp.js
+++ client/src/Pages/SignUp/SignUp.js
@@ -5,6 +5,9 @@ import API from "../../utils/API";
 //import { Input, TextArea, FormBtn } from "../../components/Form";
 import { Form, Button, FormGroup, Label, Input } from 'reactstrap';
 import { GithubLoginButton } from "react-social-login-buttons";
+import Footer from '../../components/Footer/Footer';
+
+
 // npm install bootstrap reactstrap react-social-login-buttons
 
 
@@ -53,52 +56,52 @@ class SignUp extends Component {
     render() {
         return (
 
+            <React.Fragment>
 
 
+                <Form className="signup-form">
+                    <h1 className="text-center">Sign Up To DevsLink</h1>
+                    <FormGroup>
+                        <Label>User Name</Label>
+                        <Input
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleInputChange}
+                            type="User Name"
+                            placeholder="User Name (required)"
+                        />
+                        <Label>Email</Label>
+                        <Input
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleInputChange}
+                            type="email"
+                            placeholder="Email (required)"
+                        />
+                        <Label>Password</Label>
+                        <Input
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleInputChange}
+                            type="password"
+                            placeholder="Password (required)"
+                        />
 
-
-            <Form className="signup-form">
-                <h1 className="text-center">Sign Up To DevsLink</h1>
-                <FormGroup>
-                    <Label>User Name</Label>
-                    <Input
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.handleInputChange}
-                        type="User Name"
-                        placeholder="User Name (required)"
-                    />
-                    <Label>Email</Label>
-                    <Input
-                        name="email"
-                        value={this.state.email}
-                        onChange={this.handleInputChange}
-                        type="email"
-                        placeholder="Email (required)"
-                    />
-                    <Label>Password</Label>
-                    <Input
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleInputChange}
-                        type="password"
-                        placeholder="Password (required)"
-                    />
-
-                    <button className="btn-lg btn-dark btn-block" id="signupbtn"
-                        disabled={!(this.state.username && this.state.email && this.state.password)}
-                        onClick={this.handleFormSubmit}
-                    >
-                        Create User Profile
+                        <button className="btn-lg btn-dark btn-block" id="signupbtn"
+                            disabled={!(this.state.username && this.state.email && this.state.password)}
+                            onClick={this.handleFormSubmit}
+                        >
+                            Create User Profile
                     </button>
-                    <div className="text-center pt-3">
-                        Or Use GitHub
+                        <div className="gitText text-center pt-3">
+                            Or Use GitHub
                             </div>
-                    <GithubLoginButton className="mt-3 mb-3"></GithubLoginButton>
-                </FormGroup>
-            </Form>
-
-
+                        <GithubLoginButton className="mt-3 mb-3"></GithubLoginButton>
+                    </FormGroup>
+                </Form>
+                <a href="/"><button type="button" class="backButton "> &lt;</button></a>
+                <Footer />
+            </React.Fragment>
         );
     }
 }
