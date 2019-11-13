5f849edde2b37ec6be4855f4eddd4c43b4d5a243
Sunny <loganayers2013@yahoo.com>
2019-11-12 19:04:48 -0600

    changes to signup

--- client/src/Pages/MainPage/MainPage.js
+++ client/src/Pages/MainPage/MainPage.js
@@ -19,43 +19,39 @@ class MainPage extends Component {
     }
 
     render() {
-        if (this.state.acquiredPath) {
-            return <Redirect
-                to={{ pathname: this.state.acquiredPath }}
-            />
-
-
-        } else {
-            return (
-
-
-                < div >
-
-                    <Navbar />
-                    <div className='main-page-container'>
-                        <div className='main-page-row'>
-
-                            <a href="/devsearch"> <img src={DevSearch} className="cards" id="cardSearch" alt="dev search" /></a>
-
-
-                            <a href="/devworks"> <img src={DevWorks} className="cards" id="cardWorks" alt="dev works" /></a>
-
-
-
-                            <a href="/user">  <img src={UserProfile} className="cards" id="cardUser" alt="profile" /></a>
-
-
-
-                            <a href="/project">  <img src={Project} className="cards" id="cardProject" alt="project" /></a>
-                        </div>
-                    </div>
-
-
-                    <Footer />
-                </div >
-            )
-
+        if (!this.props.loggedIn) {
+            return <Redirect to={{ pathname: '/' }} />
         }
+        return (
+
+
+            < div >
+
+                <Navbar />
+                <div className='main-page-container'>
+                    <div className='main-page-row'>
+
+                        <a href="/devsearch"> <img src={DevSearch} className="cards" id="cardSearch" alt="dev search" /></a>
+
+
+                        <a href="/devworks"> <img src={DevWorks} className="cards" id="cardWorks" alt="dev works" /></a>
+
+
+
+                        <a href="/user">  <img src={UserProfile} className="cards" id="cardUser" alt="profile" /></a>
+
+
+
+                        <a href="/project">  <img src={Project} className="cards" id="cardProject" alt="project" /></a>
+                    </div>
+                </div>
+
+
+                <Footer />
+            </div >
+        )
+
     }
 }
+
 export default MainPage;
