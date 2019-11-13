5af981b0fe5b52e423fbb8c8e1dd447f0b4ee8c2
Sunny <loganayers2013@yahoo.com>
2019-11-13 14:39:51 -0600

    heroku deployment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 const session = require('express-session');
 
@@ -28,6 +29,10 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
 }
 // Add routes, both API and view
 app.use(routes);
